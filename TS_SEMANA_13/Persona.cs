d0fd344c1c0b8b6ca8fb0dc864b19feac1fa83e1
Ricardo <jrguerram457@gmail.com>
2023-04-27 18:47:57 -0600

    Ejercicio 1

---
+++ TS_SEMANA_13/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS_SEMANA_13
+{
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private double estatura;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public double Estatura { get => estatura; set => estatura = value; }
+
+        public void CrearPersona(string Nombre, string Apellido, int Edad, double Estatura)
+        {
+            nombre = Nombre;
+            apellido= Apellido;
+            edad = Edad;
+            estatura = Estatura;
+        }
+       
+        public void MostrarDatos() 
+        {
+            Console.WriteLine($"Nombre: {nombre}");
+            Console.WriteLine($"Apellido: {apellido}");
+            Console.WriteLine($"Edad: {edad}");
+            Console.WriteLine($"Altura: {estatura}m");
+        }
+    }
+}
---
+++ TS_SEMANA_13/Program.cs
@@ -0,0 +1,15 @@
+﻿using TS_SEMANA_13;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("TRABAJO SUPERVISADO SEMANA 13 RICARDO GUERRA 1234123");
+        Persona persona = new Persona();
+        persona.CrearPersona("Ricardo", "Guerra", 17, 1.66);
+        persona.MostrarDatos();
+
+        Console.ReadKey();
+    }
+
+}
